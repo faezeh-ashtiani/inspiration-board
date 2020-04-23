52c71f477ec59d81dd42cf6c1d2cd28460cfb300
peachmakkoli <sara.lee.higgins@gmail.com>
2020-04-23 15:46:49 -0700

    adds text and emoji props to Card, begins working on JSX classNames and div structure

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,16 +4,18 @@ import emoji from 'emoji-dictionary';
 
 import './Card.css';
 
-const Card = () => {
+const Card = ({ text, emoji }) => {
   return (
-    <div className="card">
-      Card
+    <div className="card card__content">
+      {text}
+      {emoji}
     </div>
   )
 }
 
 Card.propTypes = {
-
+  text: PropTypes.string,
+  emoji: PropTypes.string // TODO - is this really a string?
 };
 
 export default Card;
