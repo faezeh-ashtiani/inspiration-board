75be0ac05301308e3c153c9c16ab11cb8c687307
peachmakkoli <sara.lee.higgins@gmail.com>
2020-04-24 08:26:09 -0700

    prints error message for shallow errors like boardName doesn't exist, cleans up comments

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,25 +1,25 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
-import CARD_DATA from '../data/card-data.json';
+// import CARD_DATA from '../data/card-data.json';
 
 const Board = ({ url, boardName }) => {
-  // iterate over card_data, grab each object, pass into card component
-  // TODO: Add key to card
+  // TODO: add helper functions? think about the big picture before we get down to the nitty gritty
 
   const [ cardList, setCardList ] = useState([]);
   const [ errorMessage, setErrorMessage ] = useState(null);
 
+  // introduce helper function so this can be called on each data update
   useEffect( () => {
     axios.get(`${url}${boardName}/cards`)
     .then((response) => {
       setCardList(response.data);
     }).catch((error) => {
-      setErrorMessage(error.cause);
+      setErrorMessage(error.response.data.cause); // massage data into better format. build more robust error handling that handles errors at a higher level (i.e., bad url)?
     })
   }, [ url, boardName ]);
 
@@ -35,15 +35,17 @@ const Board = ({ url, boardName }) => {
 
   return (
     <div>
+      <div>{errorMessage}</div>
       <div className="board">
         {cards}
       </div>
-        
     </div>
   )
 };
-Board.propTypes = {
 
+Board.propTypes = {
+  url: PropTypes.string.isRequired,
+  boardName: PropTypes.string.isRequired,
 };
 
 export default Board;
