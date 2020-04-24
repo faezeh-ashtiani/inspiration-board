e300f3f91ea4705f4a830add5c9b9ce3ab5220d4
Leah Hughes <leahwhughes@gmail.com>
2020-04-24 15:57:30 -0700

    move of state and other stuff up to App from Board in progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import './App.css';
+import axios from 'axios';
 import Board from './components/Board';
 
 const App = () => {
-  const [boardName, setBoardName] = useState(`tofu-tofu-too-too-too`)
+  const [ boardName, setBoardName ] = useState(`tofu-tofu-too-too-too`)
+  const [ cardList, setCardList ] = useState([]);
+  const [ errorMessage, setErrorMessage ] = useState(null);
 
+  const url = "https://inspiration-board.herokuapp.com/"
+
+  const getCards = useCallback(() => {
+    axios.get(`${url}boards/${boardName}/cards`)
+    .then((response) => {
+      setCardList(response.data);
+    }).catch((error) => {
+      setErrorMessage(error.response.data.cause); // massage data into better format. build more robust error handling that handles errors at a higher level (i.e., bad url)?
+    });
+  }, [url, boardName]);
+
+  useEffect( getCards, [ getCards ]);
+
+  const deleteCard = (id) => {
+    axios.delete(`${url}cards/${id}`)
+    .then((/*response*/) => { 
+      getCards();
+    })
+    .catch((error) => {
+      setErrorMessage(error.response.data.cause);
+    });
+  }
+
+  const postCard = (card) => {
+    console.log('were in postCard');
+    axios.post(`${url}boards/${boardName}/cards`, card)
+    .then((/*response*/) => { 
+      getCards();
+      console.log('were in .then');
+    })
+    .catch((error) => {
+      console.log(error)
+      setErrorMessage(error.response.data.cause);
+    });
+  }
   
   return (
     <section>
@@ -12,8 +50,13 @@ const App = () => {
         <h1 className="header__h1"><span className="header__text">Inspiration Board</span></h1>
       </header>
       <Board
-        url="https://inspiration-board.herokuapp.com/"
-        boardName={boardName}
+        cardList={cardList}
+        onDeleteCardCallBack={deleteCard}
+        onPostCardCallback={postCard}
+        errorMessage={errorMessage}
+
+        // url="https://inspiration-board.herokuapp.com/"
+        // boardName={boardName}
       />
     </section>
   );
--- src/components/Board.js
+++ src/components/Board.js
@@ -7,49 +7,49 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 // import CARD_DATA from '../data/card-data.json';
 
-const Board = ({ url, boardName }) => {
+const Board = ({ cardList, onDeleteCardCallBack, onPostCardCallback, errorMessage }) => {
   // TODO: add helper functions? think about the big picture before we get down to the nitty gritty
 
-  const [ cardList, setCardList ] = useState([]);
-  const [ errorMessage, setErrorMessage ] = useState(null);
+  // const [ cardList, setCardList ] = useState([]);
+  // const [ errorMessage, setErrorMessage ] = useState(null);
 
-  const getCards = useCallback(() => {
-    axios.get(`${url}boards/${boardName}/cards`)
-    .then((response) => {
-      setCardList(response.data);
-    }).catch((error) => {
-      setErrorMessage(error.response.data.cause); // massage data into better format. build more robust error handling that handles errors at a higher level (i.e., bad url)?
-    });
-  }, [url, boardName]);
+  // const getCards = useCallback(() => {
+  //   axios.get(`${url}boards/${boardName}/cards`)
+  //   .then((response) => {
+  //     setCardList(response.data);
+  //   }).catch((error) => {
+  //     setErrorMessage(error.response.data.cause); // massage data into better format. build more robust error handling that handles errors at a higher level (i.e., bad url)?
+  //   });
+  // }, [url, boardName]);
 
   // introduce helper function so this can be called on each data update
-  useEffect( getCards, [ getCards ]);
+  // useEffect( getCards, [ getCards ]);
 
   // DELETE https://inspiration-board.herokuapp.com/cards/:card_id
-  const deleteCard = (id) => {
-    axios.delete(`${url}cards/${id}`)
-    .then((/*response*/) => { 
-      getCards();
-    })
-    .catch((error) => {
-      setErrorMessage(error.response.data.cause);
-    });
-  }
+  // const deleteCard = (id) => {
+  //   axios.delete(`${url}cards/${id}`)
+  //   .then((/*response*/) => { 
+  //     getCards();
+  //   })
+  //   .catch((error) => {
+  //     setErrorMessage(error.response.data.cause);
+  //   });
+  // }
 
   // postCard helper function, params: text (string), emoji (string) passed in from NewCardForm 
   // axios post request, url: https://inspiration-board.herokuapp.com/boards/:board_name/cards
   // then: call getCards
   // catch error, same as everything else!!!
 
-  const postCard = (card) => {
-    axios.post(`${url}boards/${boardName}/cards`, card)
-    .then((/*response*/) => { 
-      getCards();
-    })
-    .catch((error) => {
-      setErrorMessage(error.response.data.cause);
-    });
-  }
+  // const postCard = (card) => {
+  //   axios.post(`${url}boards/${boardName}/cards`, card)
+  //   .then((/*response*/) => { 
+  //     getCards();
+  //   })
+  //   .catch((error) => {
+  //     setErrorMessage(error.response.data.cause);
+  //   });
+  // }
 
   // put this into a separate helper function that we can call inline when we render
   // can massage the data into a more friendly format like card.text
@@ -59,15 +59,15 @@ const Board = ({ url, boardName }) => {
       emojiText={card.card.emoji}
       id={card.card.id}
       key={card.card.id}
-      onDeleteCallback={deleteCard}
+      onDeleteCallback={onDeleteCardCallBack}
     />
   );
 
   return (
     <div>
-      <div>{errorMessage}</div>
+      <div className="validation-errors-display">{errorMessage}</div>
       <div className="board">
-        <NewCardForm onPostCardCallback={postCard}/>
+        <NewCardForm onPostCardCallback={onPostCardCallback}/>
         {cards}
       </div>
       
@@ -76,8 +76,8 @@ const Board = ({ url, boardName }) => {
 };
 
 Board.propTypes = {
-  url: PropTypes.string.isRequired,
-  boardName: PropTypes.string.isRequired,
+  // url: PropTypes.string.isRequired,
+  // boardName: PropTypes.string.isRequired,
 };
 
 export default Board;
