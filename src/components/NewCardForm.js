02ec42d2bfff759918c807ecca7c6884ecefc32f
peachmakkoli <sara.lee.higgins@gmail.com>
2020-04-26 12:37:15 -0700

    cleans up comments, adds emojis, adds helper function for rendering options in emoji menu

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -3,33 +3,39 @@ import PropTypes from 'prop-types';
 import emoji from 'emoji-dictionary';
 import './NewCardForm.css';
 
-const EMOJI_LIST = ["", "heart_eyes", "beer", "clap", "sparkling_heart", "heart_eyes_cat", "dog"]; // TODO: Add a drop-down menu for the user to select an emoji
+const EMOJI_LIST = ["", "heart_eyes", "smiling_face_with_three_hearts", "zany", "eyes", "sparkles", "sunflower", "seedling", "cherry_blossom", "beer", "clap", "heart", "sparkling_heart", "heart_eyes_cat", "dog", "sob", "fu", "hugs", "partying", "star_struck", "sunglasses", "space_invader", "brain", "takeout_box", "nerd_face", "fire", "rainbow", "mask"];
 
 const NewCardForm = ({ onPostCardCallback }) => {
+  
   const [card, setCard] = useState({
     text: "",
     emoji: ""
   });
 
-// TODO: Helper function to handle emoji input
-
   const onInputChange = (event) => {
-
     const newCard = {
       ...card
-    }
+    };
 
     newCard[event.target.name] = event.target.value;
     setCard(newCard);
-  }
+  };
+
+  const emojiOptions = (emojiList) => {
+    const emojis = [];
+
+    for (let i = 1; i < emojiList.length; i++) {
+      emojis.push(<option value={emojiList[i]}>{emoji.getUnicode(emojiList[i])}</option>);
+    };
+    
+    return emojis;
+  };
 
   return (
     <form className="new-card-form" onSubmit={() => onPostCardCallback(card)}>
-      <div className="new-card-form__header">
-        {/* <span>Post a Card</span> */}
-      </div>
+      <div className="new-card-form__header"></div>
       <div className="new-card-form__form">
-        {/* <label className="new-card-form__form-label" htmlFor=""></label> */}
+        <label className="new-card-form__form-label" htmlFor="text">text</label>
         <input 
           className="new-card-form__form-textarea" 
           type="text" 
@@ -38,30 +44,23 @@ const NewCardForm = ({ onPostCardCallback }) => {
           value={card.text}
           onChange={onInputChange}
         />
+        <label className="new-card-form__form-label" htmlFor="emoji">emoji</label>
         <select 
           className="new-card-form__form-select"  
           name="emoji"
           value={card.emoji}
-          onChange={onInputChange}
-         >
-           <option value={EMOJI_LIST[0]}>Select an Emoji!</option>
-           <option value={EMOJI_LIST[1]}>{emoji.getUnicode('heart_eyes')}</option>
-           <option value={EMOJI_LIST[2]}>{emoji.getUnicode('beer')}</option>
-           <option value={EMOJI_LIST[3]}>{emoji.getUnicode('clap')}</option>
-           <option value={EMOJI_LIST[4]}>{emoji.getUnicode('sparkling_heart')}</option>
-           <option value={EMOJI_LIST[5]}>{emoji.getUnicode('heart_eyes_cat')}</option>
-           <option value={EMOJI_LIST[6]}>{emoji.getUnicode('dog')}</option>
-         </select>
-        <input type='submit' className="new-card-form__form-button" value='Post a Card'/>
+          onChange={onInputChange} >
+            <option value={EMOJI_LIST[0]}>Select an Emoji!</option>
+            {emojiOptions(EMOJI_LIST)}
+        </select>
+        <input type="submit" className="new-card-form__form-button" value="Post a Card"/>
       </div>
     </form>
-      
-
   );
-}
+};
 
 NewCardForm.propTypes = {
   onPostCardCallback: PropTypes.func.isRequired
-}
+};
 
 export default NewCardForm;
