5258a1ee49496e8ad84270b9a2ea2b1f6ba957f5
peachmakkoli <sara.lee.higgins@gmail.com>
2020-04-26 14:03:10 -0700

    adds comment about failing snapshot test due to dynamic styling in Card.js

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -3,6 +3,7 @@ import { render, cleanup } from '@testing-library/react'
 import Card from './Card'; 
 
 describe('Card', () => {
+  // NOTE: Because we have a randomly generated className for the card wrapper element (e.g., "card pink", "card yellow", etc.), the snapshot test will fail when comparing line 26 in Card.js. We located a potential solution using asymmetric matchers, and a future refactor could include this. We are actively choosing to fail this snapshot test and accept any ramifications!
   test('that it matches the existing snapshot', () => {
     // Arrange-Act
     const { asFragment } = render(
