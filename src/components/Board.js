f2b8b5363dcc5fca90a01d945d3f2ae86bf462cf
peachmakkoli <sara.lee.higgins@gmail.com>
2020-04-26 12:36:23 -0700

    cleans up comments, adds semi-colons

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,10 +5,8 @@ import axios from 'axios';
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
-// import CARD_DATA from '../data/card-data.json';
 
 const Board = ({ url, boardName }) => {
-  // TODO: add helper functions? think about the big picture before we get down to the nitty gritty
 
   const [ cardList, setCardList ] = useState([]);
   const [ errorMessage, setErrorMessage ] = useState(null);
@@ -17,15 +15,14 @@ const Board = ({ url, boardName }) => {
     axios.get(`${url}boards/${boardName}/cards`)
     .then((response) => {
       setCardList(response.data);
-    }).catch((error) => {
-      setErrorMessage(error.response.data.cause); // massage data into better format. build more robust error handling that handles errors at a higher level (i.e., bad url)?
+    })
+    .catch((error) => {
+      setErrorMessage(error.response.data.cause);
     });
   }, [url, boardName]);
 
-  // introduce helper function so this can be called on each data update
   useEffect( getCards, [ getCards ]);
 
-  // DELETE https://inspiration-board.herokuapp.com/cards/:card_id
   const deleteCard = (id) => {
     axios.delete(`${url}cards/${id}`)
     .then((/*response*/) => { 
@@ -34,12 +31,7 @@ const Board = ({ url, boardName }) => {
     .catch((error) => {
       setErrorMessage(error.response.data.cause);
     });
-  }
-
-  // postCard helper function, params: text (string), emoji (string) passed in from NewCardForm 
-  // axios post request, url: https://inspiration-board.herokuapp.com/boards/:board_name/cards
-  // then: call getCards
-  // catch error, same as everything else!!!
+  };
 
   const postCard = (card) => {
     axios.post(`${url}boards/${boardName}/cards`, card)
@@ -49,10 +41,8 @@ const Board = ({ url, boardName }) => {
     .catch((error) => {
       setErrorMessage(error.response.data.cause);
     });
-  }
+  };
 
-  // put this into a separate helper function that we can call inline when we render
-  // can massage the data into a more friendly format like card.text
   const cards = cardList.map(card => 
     <Card
       text={card.card.text}
@@ -65,14 +55,15 @@ const Board = ({ url, boardName }) => {
 
   return (
     <div>
-      <div className='validation-errors-display'>{errorMessage}</div>
+      <div className="validation-errors-display">
+        {errorMessage}
+      </div>
       <div className="board">
         <NewCardForm onPostCardCallback={postCard}/>
         {cards}
       </div>
-      
     </div>
-  )
+  );
 };
 
 Board.propTypes = {
